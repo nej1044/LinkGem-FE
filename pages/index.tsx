2247c07d8a09a6b4651e910be79179a5db4cb29c
Kyounghwan01 <noh5524@gmail.com>
2022-06-18 22:29:00 +0900

    test: husky 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,6 @@ import React from 'react';
 import TestRecoilComponent from 'components/TestRecoilComponent';
 
 function Home() {
-  const res = (awdawd: string) => {
-    console.log(233);
-  };
-
-  awdawd();
-  awdawd();
-  awdawd();
-  awdawd();
-
   return (
     <>
       <div className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
