e845b931629e3df5677ca6b1683b7d1ba38f0a2a
Nyeong <68885795+cozynye@users.noreply.github.com>
2022-07-19 23:34:25 +0900

    Revert "fix : 헤더 컴포넌트 수정"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 import '../styles/reset.css';
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
-import HeaderContainer from 'components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <HeaderContainer />
       <Component {...pageProps} />
     </RecoilRoot>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,28 +6,7 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <script
-            defer
-            type="text/javascript"
-            src="https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.0.js"
-            charSet="utf-8"
-          />
-          <title>LINKGEM</title>
-          <meta name="description" content="링크들을 지인들과 공유하세요" />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-          <meta property="og:title" content="링크젬" />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="http://localhost:3000/" />
-          <meta
-            property="og:image"
-            content="/static/image/linkgem-ogimage.jpg"
-          />
-          <meta property="og:article:author" content="링크젬" />
-        </Head>
+        <Head />
         <body>
           <div id="portal" />
           <Main />
