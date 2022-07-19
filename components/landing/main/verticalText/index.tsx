e5a1030baf40f42dd8ea28f26b4633cf4eeed231
nej1044 <nej1044@naver.com>
2022-07-19 11:36:30 +0900

    fix: verticalText

--- components/landing/main/verticalText/index.tsx
+++ components/landing/main/verticalText/index.tsx
@@ -20,13 +20,6 @@ const VerticalText = () => {
         <S.Text>KEEP YOUR LINK GEM</S.Text>
         <S.Text>JUST Ctrl + C Ctrl + V</S.Text>
         <S.Text>KEEP YOUR LINK GEM</S.Text>
-        <S.Text>JUST Ctrl + C Ctrl + V</S.Text>
-        <S.Text>KEEP YOUR LINK GEM</S.Text>
-        <S.Text>JUST Ctrl + C Ctrl + V</S.Text>
-        <S.Text>KEEP YOUR LINK GEM</S.Text>
-        <S.Text>JUST Ctrl + C Ctrl + V</S.Text>
-        <S.Text>KEEP YOUR LINK GEM</S.Text>
-        <S.Text>JUST Ctrl + C Ctrl + V</S.Text>
       </S.TextWrap>
     </S.VerticalTextBlock>
   );
