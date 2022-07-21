f0ae75d0c64edd1952ab7e10170cfd24611e4677
cozynye2 <devlocozy@boomco.org>
2022-07-21 15:35:17 +0900

    fix: 빌드 에러 수정

--- components/common/NaverLogin/index.tsx
+++ components/common/NaverLogin/index.tsx
@@ -12,27 +12,26 @@ export default function LoginNaver() {
     );
   };
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const loginFormWithNaver = async () => {
-    // const naverLogin = new window.naver.LoginWithNaverId({
-    //   clientId: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID,
-    //   callbackUrl: 'http://localhost:3000/oauth/naver/callback',
-    //   isPopup: false,
-    //   loginButton: { color: 'dark mode', type: 3, height: 56 },
-    // });
-    // naverLogin.init();
-  };
+  // 이 로직은 추후에 쓸 수 있음
+  // const loginFormWithNaver = async () => {
+  // const naverLogin = new window.naver.LoginWithNaverId({
+  //   clientId: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID,
+  //   callbackUrl: 'http://localhost:3000/oauth/naver/callback',
+  //   isPopup: false,
+  //   loginButton: { color: 'dark mode', type: 3, height: 56 },
+  // });
+  // naverLogin.init();
+  // };
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const onClickNaverButton = () => {
-    // if (naverRef.current !== null && naverRef.current.children[0]) {
-    //   if (naverRef.current.children[0] instanceof HTMLAnchorElement) {
-    //     naverRef.current.children[0].click();
-    //   }
-    // }
-    window.location.href =
-      'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=GaA68400epOsIRyJ4C3r&redirect_uri=http://localhost:8080/redirect/oauth';
-  };
+  // const onClickNaverButton = () => {
+  // if (naverRef.current !== null && naverRef.current.children[0]) {
+  //   if (naverRef.current.children[0] instanceof HTMLAnchorElement) {
+  //     naverRef.current.children[0].click();
+  //   }
+  // }
+  //   window.location.href =
+  //     'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=GaA68400epOsIRyJ4C3r&redirect_uri=http://localhost:8080/redirect/oauth';
+  // };
 
   useEffect(() => {
     // loginFormWithNaver();
