d846c1f0996aa76c227d8d68cfb79e2275e62d1e
cozynye <cozynye@gmail.com>
2022-07-24 22:07:45 +0900

    feature : 푸터 추가

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,10 +1,34 @@
-import { FooterContainer, FooterTitle, FooterRight } from './Footer.style';
+import { FooterContainer, FooterLeft, FooterRight } from './Footer.style';
 
 const Footer = () => {
   return (
     <FooterContainer>
-      <FooterTitle></FooterTitle>
-      <FooterRight></FooterRight>
+      <FooterLeft>
+        <img alt="linkgem-logo" src="/static/image/linkgem-logo-white.svg" />
+        <p>Copyright 2022 Linkgem. All rights reserved.</p>
+      </FooterLeft>
+      <FooterRight>
+        <p>
+          <a
+            href="https://shimmer-columnist-0d7.notion.site/LINK-GEM-58b27dd8636d45429bb387273a90ac12"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            개인정보 처리방침
+          </a>
+          &nbsp;&nbsp;&nbsp;
+          <a
+            href="https://shimmer-columnist-0d7.notion.site/LINK-GEM-52f7b0bbd9eb4c1096096fa4b378dccf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            이용 약관
+          </a>
+        </p>
+        <p>
+          <a>Contact : linkgemkeeper@gmail.com</a>
+        </p>
+      </FooterRight>
     </FooterContainer>
   );
 };
