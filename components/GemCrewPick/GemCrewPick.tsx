36c35f96c418c3187c7aae26dd0fb829c8f4e6d8
cozynye <cozynye@gmail.com>
2022-07-31 21:35:01 +0900

    fix : 빌드 에러 수정

--- components/GemCrewPick/GemCrewPick.tsx
+++ components/GemCrewPick/GemCrewPick.tsx
@@ -17,7 +17,7 @@ function GemCrewPick() {
       <GemCrewPickOption>
         {list.map((item, index) => (
           <Link
-            key={item.id}
+            key={index}
             title={item.title}
             description={item.description}
             memos={item.memos}
@@ -37,7 +37,6 @@ export default memo(GemCrewPick);
 
 const list = [
   {
-    id: 1,
     title: 'Vite에 관한 내용',
     description:
       '이번 글은 Vite에 관한 내용입니다. 최근 Svelte와 관련한 사내 발표 내용 중 다들 재미있어 했던 Vite에 관한 이야기를 블로그 형식으로 각색하여 다시 정리해보았습니다.',
@@ -48,18 +47,16 @@ const list = [
     createDate: '22-08-10',
   },
   {
-    id: 2,
-    title: 'Prettier ESLint 쓰는사람 주목!!',
+    title: '모두를 위한 JWT',
     description:
-      '여러분 ESLint와 Prettier 아시나요? 이는 강력한 도구로 코드의 일관성을 지켜주는 코드컨벤션입니다.',
+      'JWT가 무엇이냐 물어본다면 탄생 배경부터 득과 실, 활용예시, 연관기술 등 여러 가지를 이야기해 봐야겠지만 가장 단순하게 요약한다면 아래 그림이 맞습니다. JWT는 Header + Payload + Signature로 구성된 JSON Web Token 입니다.',
     memos: '메모',
-    url: 'https://velog.io/@lokijoji2/ESLint%EB%9E%91-Prettier-%EC%93%B0%EA%B8%B0-%EA%B7%80%EC%B0%AE%EC%9C%BC%EB%A9%B4-%EC%9D%B4%EA%B1%B0-%EC%8D%A8-cak7e4uo',
+    url: 'https://velog.io/@jeonjay/%EB%AA%A8%EB%91%90%EB%A5%BC-%EC%9C%84%ED%95%9C-JWT',
     imageUrl:
-      'https://velog.velcdn.com/images/lokijoji2/post/b3e61804-e1f9-4804-99c1-e45b03bf38a4/image.gif',
+      'https://velog.velcdn.com/images/jeonjay/post/237c723e-c47d-48f9-b60b-265933fa7f49/image.png',
     createDate: '22-08-10',
   },
   {
-    id: 3,
     title: 'React + Ionic으로 iOS, 안드 앱을 만든 후기',
     description:
       '리액트로 만들어진 서비스를 앱으로 어떻게 만들지... 새로 만들긴 싫은데.. 이런 생각을 하다가 가능한 방법을 모색했습니다. 바로 떠오른 방법은 React Native였습니다. 저는 React Native에 대해서 많이 들어봤지만, 사용해 본적은 없었습니다. 지금 상황에서 React Native로 만들려면 React Native의 TextView, View로 감싸서 새롭게 개발을 해야 하며 관리 포인트가 꽤 늘어난다고 생각 하여 포기했습니다. 무엇보다 웹을 좋아하고 웹 베이스로 만들고 싶었고, 최적화 등도 웹 측면에서 해결해보고 싶었습니다.',
@@ -70,14 +67,14 @@ const list = [
     createDate: '22-08-10',
   },
   {
-    id: 4,
-    title: '낡은 코드를 뜯어고쳐봅시다.',
+    title:
+      '환상(장)의 (ElastiCache + Spring Session + Spring Security) Memory Leak',
     description:
-      '동기화봇은 구글 캘린더와 노션을 동기화하는 서비스인 Calendar2notion에서 동기화를 진행해주는 핵심 프로그램의 이름입니다. 서비스 그 자체라는 중요도에 비해 아직도 자바스크립트로 작성되어 있을 정도로 굉장히 구식입니다.',
+      'Spring Boot를 사용하기 위해 AWS EC2를 N대 이상을 사용하고 있습니다. 사용자의 인증/인가를 위해 Spring Security를 사용하고 있으며, 사용자의 로그인 정보를 모든 서버에 공유, 데이터 캐싱을 위해서 위해서 AWS ElastiCache의 Redis를 사용하고 있습니다.',
     memos: '메모',
-    url: 'https://velog.io/@phw3071/%EB%82%A1%EC%9D%80-%EC%BD%94%EB%93%9C-%EB%9C%AF%EC%96%B4%EA%B3%A0%EC%B9%98%EA%B8%B0-1',
+    url: 'https://velog.io/@betalabs/%ED%99%98%EC%83%81%EC%9D%98-%EC%A1%B0%ED%95%A9ElastiCache-Spring-Session-Spring-Security-Memory-Leak',
     imageUrl:
-      'https://velog.velcdn.com/images/phw3071/post/ecc52f1b-829c-4c59-83c3-f8f470550c2b/image.gif',
+      'https://velog.velcdn.com/images/betalabs/post/4bf661c2-b9d9-43f5-a5a2-adce3fc5fd48/image.png',
     createDate: '22-08-10',
   },
 ];
