cb35cb052c880452c182a1d38e64a50c5881cd0c
nej1044 <nej1044@naver.com>
2022-08-09 20:05:13 +0900

    feature: gembox-edit

--- components/gembox/gembox.types.ts
+++ components/gembox/gembox.types.ts
@@ -1,3 +1,5 @@
+import { Dispatch, SetStateAction } from 'react';
+
 export type IDataType = {
   id: number;
   name: string;
@@ -13,6 +15,9 @@ export type ILinkDataType = {
 
 export interface IPropsGemBoxUI {
   open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+  isEdit: boolean;
+  setIsEdit: Dispatch<SetStateAction<boolean>>;
   handleOpen: () => void;
   handleClose: () => void;
   data?: IDataType[] | any;
@@ -20,4 +25,23 @@ export interface IPropsGemBoxUI {
   totalCount: number | undefined;
   gemboxTitle: string;
   setGembox: (el?: IDataType) => () => void;
+}
+
+export interface IPropsGemCard {
+  editId: number;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+  setIsEdit: Dispatch<SetStateAction<boolean>>;
+  isEdit: boolean;
+  el: IDataType;
+  openEdit: (id: number) => () => void;
+}
+
+export interface IPropsGemboxModal {
+  isEdit: boolean;
+  setIsEdit: Dispatch<SetStateAction<boolean>>;
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+  handleClose: () => void;
+  data: IDataType[] | any;
+  editId?: number;
 }
