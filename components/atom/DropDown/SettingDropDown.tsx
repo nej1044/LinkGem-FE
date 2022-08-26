d1cb06080c44becb5dd448e1dd01d338a7149057
Eunjeong Noh <nej1044@naver.com>
2022-08-26 15:36:44 +0900

    fix: build error

--- components/atom/DropDown/SettingDropDown.tsx
+++ components/atom/DropDown/SettingDropDown.tsx
@@ -1,4 +1,10 @@
-import React, { useState, MouseEvent, useEffect } from 'react';
+import React, {
+  useState,
+  MouseEvent,
+  useEffect,
+  Dispatch,
+  SetStateAction,
+} from 'react';
 import {
   DropDownContainer,
   DropDownHeader,
@@ -12,44 +18,81 @@ import {
 
 type settingDropdownProps = {
   dropDownList: any;
+  isModal: boolean;
+  handleModal: () => void;
+  setForm: Dispatch<
+    SetStateAction<{
+      nickName: string;
+      jobName: string;
+      careerYear: string;
+      email: string;
+      name: string;
+    }>
+  >;
+  info: string | number;
+  type: string;
+  form: {
+    nickName: string;
+    jobName: string;
+    careerYear: string;
+    email: string;
+    name: string;
+  };
 };
 
-function SettingDropdown({ dropDownList }: settingDropdownProps) {
+function SettingDropdown({
+  dropDownList,
+  isModal,
+  handleModal,
+  setForm,
+  info,
+  type,
+  form,
+}: settingDropdownProps) {
   const [isOpen, setIsOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState('');
+  const [selectedOption, setSelectedOption] = useState<string | number>('');
   const [inputText, setInputText] = useState('');
 
   const toggling = (e: MouseEvent) => {
     e.preventDefault();
     setIsOpen(!isOpen);
+    handleModal();
     setInputText('');
   };
 
   const onOptionClicked = (value: string) => () => {
     setSelectedOption(value);
+    handleModal();
     setIsOpen(false);
+    const _form = { ...form };
+    if (type === 'careerYear') {
+      console.log('연차');
+      setForm({ ..._form, careerYear: value });
+    } else if (type === 'jobName') {
+      console.log('직업');
+      setForm({ ..._form, jobName: value });
+    }
   };
 
   const changeJobInputText = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputText(e.target.value);
     setIsOpen(false);
   };
-
   useEffect(() => {
-    setIsOpen(false);
-  }, []);
-
+    setSelectedOption(typeof info === 'number' ? info + '년' : info);
+  }, [info]);
   return (
     <DropDownContainer>
       <DropDownHeader onClick={toggling}>
         <DropDownHeaderText>
-          {(selectedOption.includes('하시는') && (
-            <InputText
-              value={inputText}
-              onChange={changeJobInputText}
-              placeholder="하시는 일을 입력해 주세요"
-            />
-          )) ||
+          {(typeof selectedOption === 'string' &&
+            selectedOption.includes('하시는') && (
+              <InputText
+                value={inputText}
+                onChange={changeJobInputText}
+                placeholder="하시는 일을 입력해 주세요"
+              />
+            )) ||
             selectedOption}
         </DropDownHeaderText>
 
@@ -59,7 +102,7 @@ function SettingDropdown({ dropDownList }: settingDropdownProps) {
           <ImageButton src="/images/icons/Arrow-Top.svg" alt="close" />
         )}
       </DropDownHeader>
-      {isOpen && (
+      {isOpen && isModal && (
         <DropDownListContainer>
           <DropDownList>
             {dropDownList.map((job: string) => (
